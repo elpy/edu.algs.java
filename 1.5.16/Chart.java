4a00081b188f2226b0a6f20a57608ffbd3a53f36
elpy <silmion@gmail.com>
2013-09-13 16:36:45 +0700

    Refactored some methods.

--- 1.5.16/Chart.java
+++ 1.5.16/Chart.java
@@ -4,23 +4,42 @@ import java.awt.geom.*;
 import javax.swing.*;
 
 public class Chart extends JComponent{
+    float x0 = 25, y0 = 540, dx = 0, dy = 0;
+    int maxX = 0, maxY = 0;
+
     private ArrayList<Integer> Points = new ArrayList<Integer>();
 
     public void paintComponent(Graphics g){
-	Graphics2D g2 = (Graphics2D)g;
-	int dx = 15, dy = 550;
+        Graphics2D g2 = (Graphics2D)g;
+        Scale();
+        DrawAxes(g2);
 
-	Line2D x = new Line2D.Float(new Point2D.Double(dx + 0, dy + 0), new Point2D.Double(dx + 0, dy - 530));
-	Line2D y = new Line2D.Float(new Point2D.Double(dx + 0, dy + 0), new Point2D.Double(dx + 765, dy + 0));	
-	g2.draw(x);
-	g2.draw(y);					
+        for (int i = 0; i < Points.size(); i++){
+            g2.draw(new Ellipse2D.Float(x0 + (i + 1) * dx, y0 - Points.get(i) * dy, 2, 2));
+        }
+    }
 
-	for (int i = 0; i < Points.size(); i++){
-	    g2.draw(new Ellipse2D.Float(i * 10, Points.get(i) * 10, 2, 2));	
-	}
-    } 
+    private void DrawAxes(Graphics2D g){
+        g.drawString("0", x0 - 10, y0 + 5);
+        g.drawString(String.valueOf(maxX), x0 + 510, y0 + 10);
+        g.drawString(String.valueOf(maxY), x0 - 10, y0 - 510);
+
+        Line2D x = new Line2D.Float(new Point2D.Double(x0, y0), new Point2D.Double(x0 + 505, y0));
+        Line2D y = new Line2D.Float(new Point2D.Double(x0, y0), new Point2D.Double(x0, y0 - 505));
+        g.draw(x);
+        g.draw(y);
+    }
 
     public void AddPoint(int y){
-	Points.add(y);
+        Points.add(y);
+
+        if (y > maxY)
+            maxY = y;
+    }
+
+    private void Scale(){
+        maxX = Points.size();
+        dx = 500f / maxX;
+        dy = 500f / maxY;
     }
 }
--- 1.5.16/QuickFind.java
+++ 1.5.16/QuickFind.java
@@ -10,89 +10,77 @@ public class QuickFind{
     private int Count;
 
     public QuickFind(int n){
-	Count = n;
-	Id = new int[n];
+        Count = n;
+        Id = new int[n];
 
-	for (int i = 0; i < n; i++)
-	    Id[i] = i;
+        for (int i = 0; i < n; i++)
+            Id[i] = i;
     }
 
     private int Find(int v){
-	return Id[v];
+	    return Id[v];
     }
 
     public boolean IsConnected(int p, int q){
-	return Find(p) == Find(q);
+	    return Find(p) == Find(q);
     }
 
     private int Union(int p, int q){
-	int pRoot = Find(p);
-	int qRoot = Find(q);
-	int cost = 2;
+        int pRoot = Find(p);
+        int qRoot = Find(q);
+        int cost = 2;
 
-	if (qRoot == pRoot)
-	    return cost;
+        if (qRoot == pRoot)
+            return cost;
 
-	for (int i = 0; i < Id.length; i++)
-	    if (Id[i] == pRoot){
-		Id[i] = qRoot;
-		cost += 2;
-	    } 
-	    else cost++;
+        for (int i = 0; i < Id.length; i++)
+            if (Id[i] == pRoot){
+                Id[i] = qRoot;
+                cost += 2;
+            }
+            else cost++;
 
-	Count--;
+        Count--;
 
-	return cost;
+        return cost;
     }
     
     @Override
     public String toString(){
-	return "Array: " + Arrays.toString(Id) + "\nCount: " + Count;
+	    return "Array: " + Arrays.toString(Id) + "\nCount: " + Count;
     }
-
 
     //static
 
     public static void main(String[] args){
-	EventQueue.invokeLater(new Runnable(){
-		public void run(){
-		    JFrame frame = new JFrame();
-		    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		    frame.setTitle("QuickFind");
-		    frame.setSize(800, 600);
+        EventQueue.invokeLater(new Runnable(){
+            public void run(){
+                int n = 100000;
+                float perPercent = n * 0.01f;
 
-		    QuickFind qf = new QuickFind(10);
-		    int totalCost = 0, cost = 0;
-		    
-		    Chart chart = new Chart();
-		    
-		    cost = qf.Union(4, 5);
-		    chart.AddPoint(cost);
-		    totalCost += cost;
+                JFrame frame = new JFrame();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setTitle("QuickFind");
+                frame.setSize(600, 600);
 
-		    cost = qf.Union(6, 7);
-		    chart.AddPoint(cost);
-		    totalCost += cost;
+                QuickFind qf = new QuickFind(n);
+                int totalCost = 0, cost = 0;
 
-		    cost = qf.Union(7, 8);
-		    chart.AddPoint(cost);
-		    totalCost += cost;
+                Chart chart = new Chart();
 
-		    cost = qf.Union(5, 6);
-		    chart.AddPoint(cost);
-		    totalCost += cost;
+                Random randomGenerator = new Random();
+                for (int i = 0; i < n; i++){
+                    cost = qf.Union(randomGenerator.nextInt(n), randomGenerator.nextInt(n));
+                    chart.AddPoint(cost);
+                    totalCost += cost;
 
-		    cost = qf.Union(0, 1);
-		    chart.AddPoint(cost);
-		    totalCost += cost;
+                    if (i % perPercent == 0)
+                        System.out.println("Processed: " + 100f * i / n + "%");
+                }
 
-		    cost = qf.Union(1, 8);
-		    chart.AddPoint(cost);
-		    totalCost += cost;
-
-		    frame.add(chart);
-		    frame.setVisible(true);
-		}
-	});
+                frame.add(chart);
+                frame.setVisible(true);
+            }
+        });
     }
 }
