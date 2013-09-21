39ee224c7c9c4c21726898e1bc34776e186ccc9a
elpy <silmion@gmail.com>
2013-09-21 18:36:41 +0700

    fixed pairs generation

--- 1.5.18/RandomGrid.java
+++ 1.5.18/RandomGrid.java
@@ -29,10 +29,14 @@ public class RandomGrid{
 		RandomBag<Connection> bag = new RandomBag<Connection>();
 		System.out.printf("Generating for n = %d... ", n);
 		
-		for (int i = 0; i < n; i++){
-			for (int j = 0; j < n; j++){
-				if (i != j)
-					bag.Add(new Connection(i, j));	
+		
+		for (int i = 0; i != n; i++){
+			for (int j = 0; j != n; j++){
+				if ((i + 1) != n)
+					bag.Add(new Connection(j * n + i, j * n + i + 1));
+				
+				if ((j + 1) != n)
+					bag.Add(new Connection(j * n + i, (j + 1) * n + i));				
 			}
 		}
 
